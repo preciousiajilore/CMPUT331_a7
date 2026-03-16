88a59d8d0c587ab30c75270a7133a32bfed925a9
Precious Ajilore <precious.ajilore04@gmail.com>
2026-03-16 17:11:40 -0600

    Implement p1, p2, p3, p4

--- a7p1.py
+++ a7p1.py
@@ -4,7 +4,7 @@
 #
 # CMPUT 331 Student Submission License
 # Version 1.0
-# Copyright 2026 <<Insert your name here>>
+# Copyright 2026 Precious Ajilore
 #
 # Redistribution is forbidden in all circumstances. Use of this software
 # without explicit authorization from the author is prohibited.
@@ -36,19 +36,108 @@ Assignment 7 Problem 1
 
 from sys import flags
 
+def vigenereEncrypt(key: str, plaintext: str) -> str:
+    """
+    Encrypt the plaintext using the Vigenere cipher with the given key
+    """
+    ciphertext = ""
+    for i, c in enumerate(plaintext):
+        if c.isalpha():
+            #c = (p + k) % 26
+            #shift is the amount we need to shift the plaintext character to get the ciphertext character
+            shift = ord(key[i % len(key)].upper()) - ord('A')
+
+            encrypted_char = chr((ord(c.upper()) - ord('A') + shift) % 26 + ord('A'))
+            ciphertext += encrypted_char
+        else:
+            ciphertext += c
+    return ciphertext
+
+def firstRepeatedTrigramIndex(s: str, start: int = 0) -> int:
+    """
+    Return the index of the first repeated trigram in the string s
+    If there are no repeated trigrams, return -1
+    """
+    for i in range(start, len(s) - 2):
+        trigram = s[i:i+3]
+        if s.find(trigram, i + 1) != -1:
+            return i
+    return -1
+
+def hasRepeatedTrigram(s: str) -> bool:
+    for i in range(len(s) - 2):
+        trigram = s[i:i+3]
+        if s.find(trigram, i + 1) != -1:
+            return True
+    return False
+
+ 
 
 def antiKasiski(key: str, plaintext: str) -> str:
     """
+    given a key and plaintext, insert X characters into the plaintext so
+    that after encryption there are no repeated trigrams in the ciphertext
+    then return the ciphertext
+
+    idea:
+    if a trigram appears multiple times, insert an X right after the first occurrence of the trigram in the plaintext
+    you inserted after a trigram starting at index i, do not go back and do insertions 
+    for repeated subsequences whose first occurrence is before i
     Thwart Kasiski examination 
     """
-    raise NotImplementedError()
+    """
+    Loop:
+    1. Encrypt the plaintext using the key
+    2. Find the first repeated trigram in the ciphertext
+    3. Insert an X right after the first occurrence of the repeated trigram in the plaintext
+    4. Re-encrypt
+    5. Repeat until there are no repeated trigrams in the ciphertext
+    """
+
+    modified_plaintext = plaintext
+    start_index = 0
+
+    while True:
+        #encrypt the modified plaintext
+        ciphertext = vigenereEncrypt(key, modified_plaintext)
+        #find the first repeated trigram in the ciphertext
+        index = firstRepeatedTrigramIndex(ciphertext, start_index)
+        if index == -1:
+            #no repeated trigrams, we are done
+            return ciphertext
+        
+        insert_index = index + 3 #insert after the first occurrence of the repeated trigram
+        modified_plaintext = (
+            modified_plaintext[:insert_index] + 'X' + modified_plaintext[insert_index:]
+        )
+
+        start_index = index
+
 
 
 def test():
     "Run tests"
     # TODO: test thoroughly by writing your own regression tests
     # This function is ignored in our marking
+    # helper tests
+    assert vigenereEncrypt("A", "HELLO") == "HELLO"
+    assert vigenereEncrypt("B", "ABC") == "BCD"
+    assert vigenereEncrypt("ABC", "THE") == "TIG"
 
+    assert firstRepeatedTrigramIndex("ABCDEFG", 0) == -1
+    assert firstRepeatedTrigramIndex("ABCDEFGABC", 0) == 0
+    assert firstRepeatedTrigramIndex("TIGTIGXYZ", 0) == 0
+
+    # exact sample test from assignment
+    result1 = antiKasiski("ABC", "THETESTCASETHATHASTHEREPEAT")
+    assert result1 == "TIGXUGSUEATGTICXUJATVHFTEQGAU"
+
+    result2 = antiKasiski("ABC", "THECATTHEMOUSETHEDOG")
+    assert result2 == "TIGXYEAUVHFOOVUEUJEEQG"
+
+    # property checks
+    assert not hasRepeatedTrigram(result1)
+    assert not hasRepeatedTrigram(result2)
 
 # Invoke test() if called via `python3 a7p1.py`
 # but not if `python3 -i a7p1.py` or `from a7p1 import *`
--- a7p234.py
+++ a7p234.py
@@ -4,7 +4,7 @@
 #
 # CMPUT 331 Student Submission License
 # Version 1.0
-# Copyright 2026 <<Insert your name here>>
+# Copyright 2026 Precious Ajilore
 #
 # Redistribution is forbidden in all circumstances. Use of this software
 # without explicit authorization from the author is prohibited.
@@ -34,6 +34,7 @@
 Assignment 7 Problems 2, 3, and 4
 """
 
+from random import sample
 import re
 from sys import flags
 
@@ -41,8 +42,30 @@ from sys import flags
 def stringIC(text: str) -> float:
     """
     Compute the index of coincidence (IC) for text
+    1. count how many times each letter A-Z appears in text
+    2. compute the IC using the formula:
+       IC = (sum of n_i * (n_i - 1)) 
+       where n_i is the count of letter i and N is the total number of letters in text
+    3. Divide the sum by N * (N - 1) to get the final IC value
+    4. Return the IC value as a float
     """
-    raise NotImplementedError()
+    n = len(text)
+    if n < 2:
+        return 0.0
+    
+    letter_counts = [0] * 26
+
+    for ch in text:
+        letter_counts[ord(ch) - ord('A')] += 1
+    
+    numerator = 0
+    for count in letter_counts:
+        numerator += count * (count - 1)
+
+    denominator = n * (n - 1)
+
+    return numerator / denominator
+
 
 
 def subseqIC(ciphertext: str, keylen: int) -> float:
@@ -50,7 +73,16 @@ def subseqIC(ciphertext: str, keylen: int) -> float:
     Return the average IC of ciphertext for 
     subsequences induced by a given a key length
     """
-    raise NotImplementedError()
+    total_ic = 0.0
+
+    #start with 1 because the first subsequence is the 1st letter of each key length set of letters in text
+    for i in range(1, keylen + 1):
+        #extract the subsequence of letters corresponding to the current key length
+        nth_subseq = getNthSubkeysLetters(i, keylen, ciphertext)
+        #compute the IC of the subsequence and add it to the total IC
+        total_ic += stringIC(nth_subseq)
+    
+    return total_ic / keylen
 
 
 def keyLengthIC(ciphertext: str, n: int) -> list:
@@ -58,7 +90,24 @@ def keyLengthIC(ciphertext: str, n: int) -> list:
     Return the top n keylengths ordered by likelihood of correctness
     Assumes keylength <= 20
     """
-    raise NotImplementedError()
+    #compute the subsedIC for each
+    #store both the key length and its score 
+    #sort by:
+     # higher ic first 
+     # if tie, shorter key length first
+    # return the top n key lengths
+    #assignment says to test key lengths from 1 to 20
+
+    scores = []
+
+    for keylen in range(1, 21):
+        average_ic = subseqIC(ciphertext, keylen)
+        scores.append((keylen, average_ic))
+    
+    #sort the scores
+    scores.sort(key=lambda x: (-x[1], x[0]))
+
+    return [keylen for keylen, _ in scores[:n]]
 
 
 def getNthSubkeysLetters(nth: int, keyLength: int, message: str):
@@ -84,7 +133,19 @@ def test():
     assert stringIC("ABA") == 1 / 3
     # TODO: test thoroughly by writing your own regression tests
     # This function is ignored in our marking
+    assert stringIC("A") == 0.0
+    assert stringIC("AA") == 1.0
+    assert stringIC("AB") == 0.0
+    assert stringIC("ABA") == 1/3
 
+    sample = "PPQCAXQVEKGYBNKMAZUHKNHONMFRAZCBELGRKUGDDMA"
+    assert subseqIC(sample, 3) == 0.03882783882783883
+    assert subseqIC(sample, 4) == 0.0601010101010101
+    assert subseqIC(sample, 5) == 0.012698412698412698
+
+    sample2 = ("PPQCAXQVEKGYBNKMAZUYBNGBALJONITSZMJYIMVRAG"
+               "VOHTVRAUCTKSGDDWUOXITLAZUVAVVRAZCVKBQPIWPOU")
+    assert keyLengthIC(sample2, 5) == [8, 16, 4, 12, 6]
 
 # Invoke test() if called via `python3 a7p234.py`
 # but not if `python3 -i a7p234.py` or `from a7p234 import *`
